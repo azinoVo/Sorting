df49911f6686e9030f85c466414f1b138b3cdfae
azinoVo <azinovo22@gmail.com>
2019-09-09 16:03:20 -0500

    added sort description- initial commit

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,3 +1,7 @@
+# Selection Sort 
+# 1- select the smallest element in the collection,swap with the first element
+# 2- Repeat the process with the 2nd smallest, 3rd smallest, etc.
+# You will need to pass
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
@@ -7,9 +11,6 @@ def selection_sort( arr ):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
              
-
-
-
         # TO-DO: swap
 
 
@@ -19,6 +20,10 @@ def selection_sort( arr ):
 
 
 # TO-DO:  implement the Bubble Sort function below
+# Start at beginning and compare each element to its right hand neighbor. 
+# If the right hand neighbor is smaller, we swap the two neighbors.
+# The above process is repeated until we pass through the entire collection without performing a single swap. 
+# With each pass, the larger elements will “bubble” toward the right hand side of the collection.
 def bubble_sort( arr ):
 
     return arr
